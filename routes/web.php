febe7ee3459224e18c0c52101c77c4b04a43b154
shadreza <shadreza100@gmail.com>
2023-02-11 01:01:58 +0600

    Adding more routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return "This is the about part";
+});
+
+Route::get('/contact', function () {
+    return "Hi let's connect";
+});
