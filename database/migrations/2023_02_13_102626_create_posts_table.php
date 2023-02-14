bc9790af6f9f10f8b8009ae70074a1ec322bfd68
shadreza <shadreza100@gmail.com>
2023-02-15 02:01:46 +0600

    polymorphic relation is progress

--- database/migrations/2023_02_13_102626_create_posts_table.php
+++ database/migrations/2023_02_13_102626_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->unsigned();
+            // $table->integer('user_id')->unsigned();
             $table->string('title')->unique();
             $table->text('content');
             $table->timestamps();
