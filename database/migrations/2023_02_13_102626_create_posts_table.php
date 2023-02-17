96ce8bd17a40a772a7b23d5b189a501801df59ab
shadreza <shadreza100@gmail.com>
2023-02-17 20:50:13 +0600

    The Migration was refreshed

--- database/migrations/2023_02_13_102626_create_posts_table.php
+++ database/migrations/2023_02_13_102626_create_posts_table.php
@@ -15,8 +15,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            // we are commenting for polymorphic relations
-            // $table->integer('user_id')->unsigned();s
+            $table->integer('user_id')->unsigned();
             $table->string('title')->unique();
             $table->text('content');
             $table->timestamps();
