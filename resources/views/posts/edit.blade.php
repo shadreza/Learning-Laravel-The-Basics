825353be4581d1c0851d26ef9fee5e874017d27c
shadreza <shadreza100@gmail.com>
2023-02-18 02:11:31 +0600

    form binding done... edit form converted

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,18 +6,19 @@
         <h4>Edit page</h4>
         {{ $post->title }}
 
-        <form method="post" action="/posts/{{ $post->id }}">
+        {{-- this is form binding and populate the form as well --}}
+        {{-- object, [method, [action, id]] --}}
+        {!! Form::model($post, ['method'=>'PATCH', 'action'=>['App\Http\Controllers\PostsController@update', $post->id]]) !!}
             @csrf
-            <input type="hidden" name="_method" value="PUT">
-            <input type="text" name="title" placeholder="Change Title" value="{{ $post->title }}">
-            <input type="submit" name="submit" value="UPDATE">
-        </form>
+            {!! Form::label('title', 'Update Title') !!}
+            {!! Form::text('title', null, ['class' => 'form-control']) !!}
+            {!! Form::submit('Update Post', ['class' => 'btn btn-info']) !!}
+        {!! Form::close() !!}
 
-        <form method="post" action="/posts/{{ $post->id }}">
+        {!! Form::open(['method'=>'DELETE', 'action'=>['App\Http\Controllers\PostsController@destroy', $post->id]]) !!}
             @csrf
-            <input type="hidden" name="_method" value="DELETE">
-            <input type="submit" value="DELETE">
-        </form>
+            {!! Form::submit('Delete Post', ['class' => 'btn btn-info']) !!}
+        {!! Form::close() !!}
 
     </div>
 
