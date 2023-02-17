1058fdd94acfef495b5dbe753c847a5d82f3db6e
shadreza <shadreza100@gmail.com>
2023-02-18 00:16:13 +0600

    titles are shown in index page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,11 @@
 
     <div>
         <h4>Index page</h4>
+        <ul>
+            @foreach ($posts as $post)
+                <li>{{ $post->title }}</li>
+            @endforeach
+        </ul>
     </div>
 
 @stop
